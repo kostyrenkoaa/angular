924bcc5615192a1066daa2645491c1749d63299e
Anatoliy Kostyrenko <akostyrenko@ocs.ru>
2021-08-19 02:17:42 +0400

    Одностаронняя привязка

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
+  name: string
+  age: number
+  colSpan: number = 2
+  src: string = 'https://i.stack.imgur.com/mA99c.jpg?s=64&g=1'
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  save(event:any) {
+    console.log(event)
+  }
 }
